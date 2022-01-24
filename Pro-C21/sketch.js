58c0bfc169521ebd992df2eec5a2850d6a43d1f1
Kenok345 <94133289+Kenok345@users.noreply.github.com>
2022-01-24 14:58:05 -0300

    Add files via upload

---
+++ Pro-C21/sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball;
+var world;
+var radius = 40;
+var groundObj;
+var leftSide, rightSide;
+
+
+function setup() {
+	createCanvas(1500, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	groundObj=new ground(width/2,670,width,20);
+
+	rightSide = new ground(1350, 600, 20, 120);
+
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2
+	}
+
+	//Create the Bodies Here. forma para criar na bibioteca bidies a forma circular
+    ball = Bodies.circle(260,100,radius/2,ball_options);
+	World.add(world,ball);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	rectMode(CENTER);
+	background(50);
+
+	groundObj.display();
+	rightSide.display();
+  
+	//para desenhar na tela a bola
+	ellipse(ball.position.x,ball.position.y,radius,radius);
+}
+
+
+
